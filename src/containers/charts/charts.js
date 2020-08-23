f1612c25ce655767be35ac069ac3d99246df8d69
Eric Laursen <ericjlaursen@gmail.com>
2020-08-23 19:19:01 -0400

    charts wrapper

---
+++ src/containers/charts/charts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ResponsiveWrapper from './../../components/ResponsiveWrapper'
+const charts = () => (
+	<ResponsiveWrapper 
+		className="lineBox"
+		dimensions={{w: 450, h: 250}}
+	>
+		<p>demo text here</p>
+	</ResponsiveWrapper>
+
+);
+
+export default charts;
