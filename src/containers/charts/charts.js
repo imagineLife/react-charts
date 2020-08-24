1a13d4d95d45b05457b6437e4e4d2ef24ea6cd82
Eric Laursen <ericjlaursen@gmail.com>
2020-08-24 06:37:58 -0400

    renders Line Component

--- src/containers/charts/charts.js
+++ src/containers/charts/charts.js
@@ -1,14 +1,45 @@
 import React from 'react';
 
-import ResponsiveWrapper from './../../components/ResponsiveWrapper'
-const charts = () => (
-	<ResponsiveWrapper 
-		className="lineBox"
-		dimensions={{w: 450, h: 250}}
-	>
-		<p>demo text here</p>
-	</ResponsiveWrapper>
+import Line from './../../components/Line'
+const Charts = () => (
+	<Line
+		className="thisLine"
+		width={450}
+		height={250}
+		margins={{t: 10, l: 10, b:10}}
+		data={{
+			xDomain: [201908, 201909, 201910, 201911, 201912, 202001],
+			yDomain:[0,40],
+			points: [
+				{
+					x: 201908,
+					y: 14
+				},
+				{
+					x: 201909,
+					y: 39
+				},
+				{
+					x: 201910,
+					y: 22
+				},
+				{
+					x: 201911,
+					y: 7
+				},
+				{
+					x: 201912,
+					y: 11
+				},
+				{
+					x: 202001,
+					y: 32
+				}
+			]
+		}}
+		type="Band"
+	/>
 
 );
 
-export default charts;
+export default Charts;
