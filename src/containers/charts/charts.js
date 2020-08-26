7bf13e7b093243c876bd018cb59de8fdb6c1328d
Eric Laursen <ericjlaursen@gmail.com>
2020-08-25 21:26:52 -0400

    begins treemap

--- src/containers/charts/charts.js
+++ src/containers/charts/charts.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import lineData from './../../data/line.json'
+import treeMapData from './../../data/treeMap.json'
 import Line from './../../components/Line'
+import Treemap from './../../components/Treemap'
 const Charts = () => {
-	const lineProps = {
-		className: "thisLine",
+
+	const dims = {
 		width: 450,
 		height: 250,
-		margins: {t: 10, l: 10, b:10},
+		margins: {t: 10, l: 10, b:10}
+	};
+
+	const lineProps = {
+		className: "thisLine",
 		data: lineData,
-		type: "Band"
+		type: "Band",
+		...dims
+	}
+	const treeMapProps = {
+		className: "treeMap",
+		data: treeMapData,
+		...dims
 	}
 
 	return (
-	<Line
-		{...lineProps}
-	/>
+		<Fragment>
+		<Line
+			{...lineProps}
+		/>
+		<Treemap
+			{...treeMapProps}
+		/>
+		</Fragment>
 )
 };
 
