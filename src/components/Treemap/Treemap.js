9813a2312e709b844d7269e5c145800aa67375fe
Eric Laursen <ericjlaursen@gmail.com>
2020-08-26 06:22:44 -0400

    init useHierarchy hook

--- src/components/Treemap/Treemap.js
+++ src/components/Treemap/Treemap.js
@@ -1,7 +1,33 @@
 import React from 'react';
 import ResponsiveWrapper from './../../components/ResponsiveWrapper';
+import { stratify, treemap } from 'd3-hierarchy';
+
+const useHierarchy = (data) => {
+  function sumBySize(d) {
+    return d.size;
+  }
+
+  //stratify data
+  var stratRootData = stratify()
+    .id((d) => d.name)
+    .parentId((d) => d.parent)(data);
+
+  let hierarched = stratRootData
+    .eachBefore((d) => {
+      d.data.id = (d.parent ? `${d.parent.data.id}.` : '') + d.data.name;
+      return d;
+    })
+    .sum(sumBySize)
+    .sort((a, b) => b.height - a.height || b.value - a.value);
+
+  treemap(hierarched);
+  return [hierarched];
+};
 
 const Treemap = ({ width, height, margins, className, data }) => {
+  const [hierachyData] = useHierarchy(data);
+  console.log('TreeMap rendering');
+  const thisThing = 'water';
   return (
     <svg className={`${className}-svg`} {...{ height, width }}>
       <g
