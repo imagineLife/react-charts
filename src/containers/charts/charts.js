465a63f489e5b450831e7ca66c83826fe77ea175
Eric Laursen <ericjlaursen@gmail.com>
2020-08-26 06:07:51 -0400

    lint

--- src/containers/charts/charts.js
+++ src/containers/charts/charts.js
@@ -1,38 +1,33 @@
 import React, { Fragment } from 'react';
-import lineData from './../../data/line.json'
-import treeMapData from './../../data/treeMap.json'
-import Line from './../../components/Line'
-import Treemap from './../../components/Treemap'
+import lineData from './../../data/line.json';
+import treeMapData from './../../data/treeMap.json';
+import Line from './../../components/Line';
+import Treemap from './../../components/Treemap';
 const Charts = () => {
+  const dims = {
+    width: 450,
+    height: 250,
+    margins: { t: 10, l: 10, b: 10 }
+  };
 
-	const dims = {
-		width: 450,
-		height: 250,
-		margins: {t: 10, l: 10, b:10}
-	};
+  const lineProps = {
+    className: 'thisLine',
+    data: lineData,
+    type: 'Band',
+    ...dims
+  };
+  const treeMapProps = {
+    className: 'treeMap',
+    data: treeMapData,
+    ...dims
+  };
 
-	const lineProps = {
-		className: "thisLine",
-		data: lineData,
-		type: "Band",
-		...dims
-	}
-	const treeMapProps = {
-		className: "treeMap",
-		data: treeMapData,
-		...dims
-	}
-
-	return (
-		<Fragment>
-			<Line
-				{...lineProps}
-			/>
-			<Treemap
-				{...treeMapProps}
-			/>
-			</Fragment>
-	)
+  return (
+    <Fragment>
+      <Line {...lineProps} />
+      <Treemap {...treeMapProps} />
+    </Fragment>
+  );
 };
 
 export default Charts;
