1f1bebb54f3cb409d343f19516c16c0dce82ac56
Eric Laursen <ericjlaursen@gmail.com>
2020-08-26 06:10:11 -0400

    lint

--- src/components/Treemap/Treemap.js
+++ src/components/Treemap/Treemap.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import ResponsiveWrapper from './../../components/ResponsiveWrapper';
 
-const Treemap = () => (<p>Treemap</p>);
+const Treemap = ({ width, height, margins, className, data }) => {
+  return (
+    <svg className={`${className}-svg`} {...{ height, width }}>
+      <g
+        className="line-group"
+        transform={`translate(${margins.l},${margins.t})`}
+      >
+        <text>test</text>
+      </g>
+    </svg>
+  );
+};
 
 export default Treemap;
