0daff4305b39ed0649d6855a76d03e0d16a495d4
Eric Laursen <ericjlaursen@gmail.com>
2020-09-03 12:24:40 -0400

    links

--- src/components/TreeLayout/TreeLayout.js
+++ src/components/TreeLayout/TreeLayout.js
@@ -3,6 +3,8 @@ import ResponsiveWrapper from './../../components/ResponsiveWrapper';
 import { stratify, tree, hierarchy } from 'd3-hierarchy';
 import { schemeDark2 } from 'd3-scale-chromatic';
 import { scaleOrdinal } from 'd3-scale';
+import { linkHorizontal } from 'd3-shape';
+import './TreeLayout.css';
 
 const useHierarchy = (data) => {
   function sumBySize(d) {
@@ -15,21 +17,50 @@ const useHierarchy = (data) => {
     .parentId((d) => d.parent)(data);
 
   const nodes = hierarchy(data, (d) => d.children);
-
-  return [stratRootData, nodes];
+  return [stratRootData, stratRootData.descendants()];
 };
 
 const TreeLayout = ({ width, height, margins, className, data }) => {
   const [stratRootData, nodes] = useHierarchy(data);
+  const treeIt = tree().size([height * 0.8, width * 0.7]);
+  const treeifiedData = treeIt(stratRootData);
 
-  const colorScale = scaleOrdinal(schemeDark2);
   return (
     <svg className={`${className}-svg`} {...{ height, width }}>
       <g
-        className="tree-layout-group"
-        transform={`translate(${margins.l},${margins.t})`}
+        className="tree-layout-g-wrapper"
+        transform={`translate(${margins.l + 75}, ${margins.t})`}
       >
-        {!stratRootData ? null : <text>Here</text>}
+        {!treeifiedData
+          ? null
+          : treeifiedData.descendants().map((d, idx) => (
+              <g
+                key={`dot-node-${idx}`}
+                className="tree-layout-group"
+                transform={`translate(${d.y},${d.x})`}
+              >
+                <circle r={2.5} />
+                <text
+                  dy={3}
+                  x={d.children ? -8 : 8}
+                  y={d.children && d.parent !== null ? -8 : 0}
+                  textAnchor={d.children ? 'end' : 'start'}
+                >
+                  {d.id}
+                </text>
+              </g>
+            ))}
+        {!treeifiedData
+          ? null
+          : treeifiedData.links().map((d, idx) => (
+              <path
+                key={`tree-layout-link-${idx}`}
+                className={`link`}
+                d={linkHorizontal()
+                  .x((d) => d.y)
+                  .y((d) => d.x)(d)}
+              ></path>
+            ))}
       </g>
     </svg>
   );
