4b4ff02bd15dd5ad822447e83d6946bf7434d9bc
Eric Laursen <ericjlaursen@gmail.com>
2020-09-06 21:04:05 -0400

    ref on the group wrapper

--- src/components/ForceLayout/ForceLayout.js
+++ src/components/ForceLayout/ForceLayout.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import ResponsiveWrapper from './../../components/ResponsiveWrapper';
 import * as dsh from 'd3-shape';
 import * as dscl from 'd3-scale';
+import * as d3F from 'd3-force';
 
 const ForceLayout = ({
   width,
@@ -12,21 +13,48 @@ const ForceLayout = ({
   className,
   barWidthPercentage
 }) => {
-  const { xDomain, yDomain, points } = data;
-
-  let lineSize = {
-    w: width - margins.l,
-    h: height - margins.t
+  const forceProperties = {
+    center: {
+      x: 0.5,
+      y: 0.5
+    },
+    charge: {
+      enabled: true,
+      strength: -30,
+      distanceMin: 1,
+      distanceMax: 2000
+    },
+    collide: {
+      enabled: true,
+      strength: 0.7,
+      iterations: 1,
+      radius: 5
+    },
+    forceX: {
+      enabled: false,
+      strength: 0.1,
+      x: 0.5
+    },
+    forceY: {
+      enabled: false,
+      strength: 0.1,
+      y: 0.5
+    },
+    link: {
+      enabled: true,
+      distance: 30,
+      iterations: 1
+    }
   };
+  const gRef = useRef();
+  const { nodes, link } = data;
 
-  let thisScale = `scale${type}`;
+  useEffect(() => {
+    console.log('gRef');
+    console.log(gRef.current);
 
-  const xScale = dscl[thisScale]().domain(xDomain).range([0, lineSize.w]);
-
-  const yScale = dscl
-    .scaleLinear()
-    .domain(yDomain)
-    .range([height - margins.b, margins.t]);
+    console.log('inside effect!');
+  });
 
   return (
     <ResponsiveWrapper dimensions={{ w: width, h: height }}>
@@ -34,6 +62,7 @@ const ForceLayout = ({
         <g
           className="bar-group"
           transform={`translate(${margins.l},${margins.t})`}
+          ref={gRef}
         ></g>
       </svg>
     </ResponsiveWrapper>
