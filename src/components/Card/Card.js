cd49b60784988a84e83264a58848f39f1055e3b1
Eric Laursen <ericjlaursen@gmail.com>
2020-09-25 06:08:10 -0400

    useDimension hook

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Card.css';
 
-const Card = ({ children, className }) => (
-  <div className={`card ${className}`}>
-    {children && children}
-    {!children && <p>empty card</p>}
-  </div>
-);
+// Hook
+import useDimensions from './../../Hooks/useDimensions';
+
+const Card = ({ children, className }) => {
+  // div will render without cardDimensions first
+  const [cardRef, cardDimensions] = useDimensions();
+
+  console.log('cardDimensions');
+  console.log(cardDimensions);
+
+  return (
+    <div className={`card ${className}`} ref={cardRef}>
+      {children && children}
+      {!children && <p>empty card</p>}
+    </div>
+  );
+};
 
 export default Card;
