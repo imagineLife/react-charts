4ff9ba123e964b061124ebb85e33a6a0d16a5edc
Eric Laursen <ericjlaursen@gmail.com>
2020-09-29 20:26:12 -0400

    logs

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,39 @@ import './Card.scss';
 
 // import gutterpadding
 import sassVars from './../../float-grid.scss';
+
 // Hook
 import useDimensions from './../../Hooks/useDimensions';
+console.log('sassVars');
+console.log(sassVars);
+const gp = parseFloat(sassVars['gutter-padding'].replace('%', '')) / 100;
+const gutter = parseInt(sassVars.gutter.replace('px', ''));
+console.log('gutter');
+console.log(gutter);
 
-const Card = ({ children, className }) => {
+const Card = ({ children, className, parentSize }) => {
   // div will render without cardDimensions first
   const [cardRef, cardDimensions] = useDimensions();
 
+  /*
+    - pull gutter-width % from sass file
+    - apply gutter-width to cardDimensions fn result
+  */
+  console.log('gp');
+  console.log(gp);
+  const parentWidthLessGutters =
+    parentSize && parentSize.width && parentSize.width;
+  console.log('parentWidthLessGutters');
+  console.log(parentWidthLessGutters);
+
+  let appliedGutter = parentWidthLessGutters * gp || null;
+  console.log('parentWidthLessGutters');
+  console.log(parentWidthLessGutters);
+
+  console.log('appliedGutter');
+  console.log(appliedGutter);
+
+  let widthLessGutter = cardDimensions.width - appliedGutter;
   return (
     <div className={`card ${className}`} ref={cardRef}>
       {children &&
